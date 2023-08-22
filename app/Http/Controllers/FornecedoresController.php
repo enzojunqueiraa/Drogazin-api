06dca9026af97e8db4cc384a5861777469100c88
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-22 12:45:19 -0300

    Fornecedores Update

---
+++ app/Http/Controllers/FornecedoresController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\FornecedoresRequest;
+use App\Models\Fornecedores;
+use Illuminate\Http\Request;
+
+class FornecedoresController extends Controller
+{
+    public function store(FornecedoresRequest $request){
+        $fornecedores = Fornecedores::create([
+            'Nome' => $request->Nome,
+            'Contato' => $request->Contato,
+            'CPF' => $request->CPF,
+            'Email' => $request->Email,
+            'InscriçãoMunicipal' => $request->InscriçãoMunicipal,
+            'País' => $request->País,
+            'CNPJ' => $request->CNPJ,
+            'Bairro' => $request->Bairro,
+            'Código' => $request->Código,
+            'Cidade' => $request->Cidade,
+            'WebSite' => $request->WebSite,
+        ]);
+        return response()->json([
+            "sucess" => true,
+            "message" => "Registered User",
+            "data"=> $fornecedores
+        ], 200);
+    }
+}
---
+++ database/migrations/2023_08_22_154037_create_fornecedores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nome', 80)->nullable(false);
+            $table->string('Contato', 15)->nullable(false);
+            $table->string('CPF', 14)->unique()->nullable(false);
+            $table->string('Email', 100)->unique()->nullable(false);
+            $table->string('InscriçãoMunicipal', 11)->nullable(false);
+            $table->string('País', 40)->nullable(false);
+            $table->string('CNPJ', 14)->unique()->nullable(false);
+            $table->string('Bairro', 30)->nullable(false);
+            $table->string('Código', 12)->unique()->nullable(false);
+            $table->string('Cidade', 40)->nullable(false);
+            $table->string('WebSite', 100)->unique()->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fornecedores');
+    }
+};
