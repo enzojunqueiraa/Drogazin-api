f98e19639fcee134c977fed044933bc2ec26b8db
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-22 12:47:36 -0300

    Fornecedores Update

--- app/Models/Fornecedores.php
+++ app/Models/Fornecedores.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedores extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'Nome',
+        'Contato',
+        'CPF',
+        'Email',
+        'InscriçãoMunicipal',
+        'País',
+        'CNPJ',
+        'Bairro',
+        'Código',
+        'Cidade',
+        'WebSite',
+    ];
 }
